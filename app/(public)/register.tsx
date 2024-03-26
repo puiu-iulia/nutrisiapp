cd7ac4dcf4d670ced65b48abba0f61d10cd81d4f
Iulia Puiu <48315224+puiu-iulia@users.noreply.github.com>
2024-03-26 16:07:58 +0200

    Refactor login and register pages

--- app/(public)/register.tsx
+++ app/(public)/register.tsx
@@ -1,13 +1,13 @@
-import { Button, StyleSheet, TextInput, View } from 'react-native';
 import React, { useState } from 'react';
+import { View } from 'tamagui';
+import AuthForm from '@/components/authForm';
+import ThemedScreen from '@/components/screen';
 // import Spinner from 'react-native-loading-spinner-overlay';
 // import { useAuth } from '../../provider/AuthProvider';
 
 const Page = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
-//   const { onRegister, onLogin } = useAuth();
+  //   const { onRegister, onLogin } = useAuth();
 
   const handleRegistration = async () => {
     // try {
@@ -23,32 +23,16 @@ const Page = () => {
   };
 
   return (
-    <View style={styles.container}>
-      {/* <Spinner visible={loading} /> */}
-
-      <TextInput placeholder="Email" value={email} onChangeText={setEmail} style={styles.inputField} />
-      <TextInput secureTextEntry placeholder="Password" value={password} onChangeText={setPassword} style={styles.inputField} />
-      <Button title="Create free account" onPress={handleRegistration} disabled={loading} />
-    </View>
+    <ThemedScreen>
+      <View f={1} jc="center">
+        {/* <Spinner visible={loading} /> */}
+        <AuthForm
+          onSubmit={handleRegistration}
+          type="register"
+        />
+      </View>
+    </ThemedScreen>
   );
 };
 
 export default Page;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 20,
-  },
-
-  inputField: {
-    height: 50,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    marginVertical: 5,
-  },
-});
