bf157b23ae7c9b336066ccd5769222da04470e5f
Iulia Puiu <48315224+puiu-iulia@users.noreply.github.com>
2024-03-27 12:44:12 +0200

    Update dependencies and add Redux store

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,13 @@ import tamaguiConfig from '@/tamagui.config';
 import { Slot, useRouter, useSegments } from 'expo-router';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { UseSelector, useSelector } from 'react-redux';
+import { Provider } from 'react-redux';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 import { TamaguiProvider, Theme } from 'tamagui';
+import { store } from '@/store/store';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -21,14 +23,11 @@ export const unstable_settings = {
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-
-
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
     InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
   });
-
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
@@ -49,22 +48,25 @@ export default function RootLayout() {
 }
 
 function InitialLayout() {
+  const token = useSelector(
+    (state: any) => state.auth.token,
+  );
+  const initialized = useSelector(
+    (state: any) => state.auth.success,
+  );
+  const router = useRouter();
+  const segments = useSegments();
 
-    const token = '';
-    const initialized = false;
-    const router = useRouter();
-    const segments = useSegments();
+  useEffect(() => {
+    if (!initialized) return;
 
-    useEffect(() => {
-      if (!initialized) return;
+    const inAuthGroup = segments[0] === '(auth)';
 
-      const inAuthGroup = segments[0] === '(auth)';
-
-      if (token && !inAuthGroup) {
-        router.replace('/(tabs)');
-      } else if (!token && inAuthGroup) {
-        router.replace('/(public)/login');
-      }
+    if (token && !inAuthGroup) {
+      router.replace('/(tabs)');
+    } else if (!token && inAuthGroup) {
+      router.replace('/(public)/login');
+    }
   }, [token, initialized]);
 
   return <Slot />;
@@ -75,8 +77,12 @@ function RootLayoutNav() {
 
   return (
     <TamaguiProvider config={tamaguiConfig}>
-      <Theme name={colorScheme == 'dark' ? 'dark' : 'light'}>
-        <InitialLayout />
+      <Theme
+        name={colorScheme == 'dark' ? 'dark' : 'light'}
+      >
+        <Provider store={store}>
+          <InitialLayout />
+        </Provider>
       </Theme>
     </TamaguiProvider>
   );
