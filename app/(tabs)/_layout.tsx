863f139b0eb7454a0811c358c8cd5af7f3311a07
Iulia Puiu <48315224+puiu-iulia@users.noreply.github.com>
2024-03-28 14:53:21 +0200

    Refactor tab layout and update icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,52 +2,46 @@ import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
 import { Pressable } from 'react-native';
-
+import { Home, BookMarked } from '@tamagui/lucide-icons';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return (
+    <FontAwesome
+      size={28}
+      style={{ marginBottom: -3 }}
+      {...props}
+    />
+  );
 }
 
 export default function TabLayout() {
-
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: '#4f6c4e',
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
-        headerShown: true,
-      }}>
+        headerShown: false,
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={'black'}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+          tabBarIcon: ({ color }) => <Home color={color} />,
         }}
       />
       <Tabs.Screen
         name="two"
         options={{
           title: 'Recipes',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <BookMarked color={color} />
+          ),
         }}
       />
     </Tabs>
