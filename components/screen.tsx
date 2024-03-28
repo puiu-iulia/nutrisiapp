110c226ee90b0244e88e17cd65e1c79e68cd75fa
Iulia Puiu <48315224+puiu-iulia@users.noreply.github.com>
2024-03-28 14:54:35 +0200

    Refactor screen component styling

--- components/screen.tsx
+++ components/screen.tsx
@@ -7,12 +7,7 @@ interface screenProps {
 
 function ThemedScreen({ children }: screenProps) {
   return (
-    <View
-      f={1}
-      theme={'nutrisi'}
-      bc={'$backgroundColor'}
-      p={8}
-    >
+    <View f={1} backgroundColor={'$green1Light'} p={16}>
       {children}
     </View>
   );
