11a21b9e03561a37b12c393a17a428340e0dbd2d
Iulia Puiu <48315224+puiu-iulia@users.noreply.github.com>
2024-03-29 15:58:55 +0200

    Refactor home screen components and add image picker

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,22 +1,46 @@
 import { useState } from 'react';
-import { StyleSheet, Image } from 'react-native';
-import { Button, View, Text, Input } from 'tamagui';
-import { Wand2, Settings2 } from '@tamagui/lucide-icons';
+import {
+  StyleSheet,
+  Platform,
+  Keyboard,
+  Pressable,
+} from 'react-native';
+import {
+  Button,
+  View,
+  Text,
+  Sheet,
+  TextArea,
+} from 'tamagui';
+import {
+  Wand2,
+  Settings2,
+  Camera,
+} from '@tamagui/lucide-icons';
 
 import ThemedScreen from '@/components/screen';
 import ThemedButton from '@/components/button';
 import ThemedInput from '@/components/input';
+import PremiumCard from '@/components/premiumCard';
+import ThemedImagePicker from '@/components/imagePicker';
+import { useGenerateRecipeMutation } from '@/store/api/recipes';
 
 export default function TabOneScreen() {
   const [ingredients, setIngredients] =
     useState<string>('');
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const [generateRecipe, { data, error, isLoading }] =
+    useGenerateRecipeMutation();
+
+  console.log(data, error, isLoading);
 
   return (
     <ThemedScreen>
       <View
         fd={'row'}
         jc={'space-between'}
-        pt={48}
+        pt={Platform.OS == 'ios' ? 48 : 24}
         ai={'center'}
       >
         <Text
@@ -30,9 +54,17 @@ export default function TabOneScreen() {
         >
           Not sure what to cook tonight?
         </Text>
-        <Settings2 size={24} color={'$gray1Dark'} />
+        <Pressable
+          onPress={() => setIsOpen(true)}
+          style={{
+            width: 24,
+            height: 24,
+          }}
+        >
+          <Settings2 size={24} color={'$gray1Dark'} />
+        </Pressable>
       </View>
-      <View p={16} br={16} bc={'$orange3Light'} mb={16}>
+      <View p={16} br={16} bc={'$yellow2Light'} mb={16}>
         <Text fontSize={16} color={'$gray1Dark'} mb={8}>
           Add your ingredients followed by a comma and we'll
           generate a delicious recipe based on them using
@@ -40,43 +72,62 @@ export default function TabOneScreen() {
         </Text>
         <ThemedInput
           value={ingredients}
-          placeholder={'Carrots, Chicken, Rice, ...'}
+          placeholder={'Carrots, Chicken, Rice, ...,'}
           onChangeText={setIngredients}
+        />
+        <ThemedImagePicker
+          onSubmit={(uri) => console.log(uri)}
         />
         <ThemedButton
           buttonTitle={'Generate Recipe'}
           icon={<Wand2 size={20} color={'white'} />}
-          onPress={() => {}}
+          onPress={() => {
+            setIsOpen(true);
+            Keyboard.dismiss();
+            generateRecipe({
+              ingredientsText: ingredients,
+            });
+          }}
         />
       </View>
-      <View
-        p={16}
-        br={16}
-        bc={'$orange5Light'}
-        fd={'row'}
-        jc={'space-between'}
+      <PremiumCard />
+      <Sheet
+        forceRemoveScrollEnabled={isOpen}
+        open={isOpen}
+        onOpenChange={setIsOpen}
+        zIndex={100_000}
+        snapPoints={[93]}
+        modal
+        dismissOnSnapToBottom
       >
-        <View f={5} jc={'center'}>
-          <Text fontSize={14} pb={4} color={'$nutrisi'}>
-            Starting at $9.99/month
-          </Text>
-          <Text
-            fontSize={16}
-            fontWeight={'bold'}
-            color={'$nutrisi'}
-          >
-            Unlimited recipes, scan ingredients from images
-            and generate weekly mealplans
-          </Text>
-        </View>
-        <View f={2} width={100} height={80}>
-          <Image
-            style={{ width: '100%', height: '100%' }}
-            resizeMode="cover"
-            source={require('./../../../assets/images/food.png')}
-          />
-        </View>
-      </View>
+        <Sheet.Overlay
+          animation="lazy"
+          enterStyle={{ opacity: 0 }}
+          exitStyle={{ opacity: 0 }}
+        />
+        <Sheet.Handle />
+        <Sheet.Frame>
+          <View p={16} bc={'white'} f={1}>
+            <Text fontSize={20} fontWeight={'bold'} pb={16}>
+              Generated Recipe
+            </Text>
+            <Text fontSize={16} color={'$gray1Dark'}>
+              {data?.data}
+            </Text>
+            <Button
+              onPress={() => setIsOpen(false)}
+              bc={'$nutrisi'}
+              color={'white'}
+              fontSize={16}
+              mb={16}
+              fontWeight={'bold'}
+              p={16}
+            >
+              Close
+            </Button>
+          </View>
+        </Sheet.Frame>
+      </Sheet>
     </ThemedScreen>
   );
 }
