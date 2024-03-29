7370053aae4cbb7ec1765571020a8daecbb4489a
Iulia Puiu <48315224+puiu-iulia@users.noreply.github.com>
2024-03-29 15:59:08 +0200

    Refactor tab one screen component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,8 @@
-import { StyleSheet } from 'react-native';
-import { Button, View, Text } from 'tamagui';
-import { Wand2 } from '@tamagui/lucide-icons';
+import React from 'react';
+import { Redirect } from 'expo-router';
 
-export default function TabOneScreen() {
-  return (
-    <View >
-      <Text style={styles.title} color={"#232924"}>Tab One</Text>
-      <Button 
-        animation='bouncy'
-        bc={'$nutrisi'}
-        color={'#fff'}
-        size={'$4'}
-        textProps={{color: '#fff', size: '$4'}}
-        icon={<Wand2 size={20} color={'#fff'}/>}
-        onPress={() => {}} >Generate</Button>
-    </View>
-  );
+function index() {
+  return <Redirect href="/(tabs)/home" />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+export default index;
