00668fc48b70a5070eea9346360fb508c1ca9404
Iulia Puiu <48315224+puiu-iulia@users.noreply.github.com>
2024-04-05 17:32:17 +0300

    Refactor code to improve performance and readability

--- components/recipeDetailsTabs.tsx
+++ components/recipeDetailsTabs.tsx
@@ -1,15 +1,25 @@
+import { current } from '@reduxjs/toolkit';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
-import type { TabsContentProps } from 'tamagui';
+import type {
+  TabsContentProps,
+  TabLayout,
+  StackProps,
+  TabsTabProps,
+} from 'tamagui';
 import {
   Button,
   H5,
+  YStack,
   Separator,
   SizableText,
   Tabs,
   XStack,
   Text,
   isWeb,
+  styled,
+  AnimatePresence,
+  View,
 } from 'tamagui';
 
 function RecipeDetailsTabs({
@@ -18,111 +28,265 @@ function RecipeDetailsTabs({
   macros,
 }: any) {
   //console.log('ingredients', steps);
+
+  const [tabState, setTabState] = useState<{
+    currentTab: string;
+    /**
+     * Layout of the Tab user might intend to select (hovering / focusing)
+     */
+    intentAt: TabLayout | null;
+    /**
+     * Layout of the Tab user selected
+     */
+    activeAt: TabLayout | null;
+    /**
+     * Used to get the direction of activation for animating the active indicator
+     */
+    prevActiveAt: TabLayout | null;
+  }>({
+    activeAt: null,
+    currentTab: 'tab1',
+    intentAt: null,
+    prevActiveAt: null,
+  });
+
+  const setCurrentTab = (currentTab: string) =>
+    setTabState({ ...tabState, currentTab });
+  const setIntentIndicator = (intentAt: any) =>
+    setTabState({ ...tabState, intentAt });
+  const setActiveIndicator = (activeAt: any) =>
+    setTabState({
+      ...tabState,
+      prevActiveAt: tabState.activeAt,
+      activeAt,
+    });
+  const { activeAt, intentAt, prevActiveAt, currentTab } =
+    tabState;
+
+  /**
+   * -1: from left
+   *  0: n/a
+   *  1: from right
+   */
+  const direction = (() => {
+    if (
+      !activeAt ||
+      !prevActiveAt ||
+      activeAt.x === prevActiveAt.x
+    ) {
+      return 0;
+    }
+    return activeAt.x > prevActiveAt.x ? -1 : 1;
+  })();
+
+  const enterVariant =
+    direction === 1
+      ? 'isLeft'
+      : direction === -1
+        ? 'isRight'
+        : 'defaultFade';
+  const exitVariant =
+    direction === 1
+      ? 'isRight'
+      : direction === -1
+        ? 'isLeft'
+        : 'defaultFade';
+
+  const handleOnInteraction: TabsTabProps['onInteraction'] =
+    (type, layout) => {
+      if (type === 'select') {
+        setActiveIndicator(layout);
+      } else {
+        setIntentIndicator(layout);
+      }
+    };
+
   return (
     <Tabs
-      defaultValue="tab1"
+      value={currentTab}
+      onValueChange={setCurrentTab}
       orientation="horizontal"
       flexDirection="column"
-      width={400}
-      height={600}
-      borderRadius="$4"
-      borderWidth="$0.25"
+      width={'100%'}
+      height={'100%'}
       overflow="hidden"
-      borderColor="$borderColor"
+      activationMode="manual"
+      // borderColor="$nutrisiDark"
+      // borderWidth={1}
     >
-      <Tabs.List
-        separator={<Separator vertical />}
-        disablePassBorderRadius="bottom"
-        aria-label="Manage your account"
-      >
-        <Tabs.Tab flex={1} value="tab1">
-          <SizableText fontFamily="$body">
-            Ingredients
-          </SizableText>
-        </Tabs.Tab>
-        <Tabs.Tab flex={1} value="tab2">
-          <SizableText fontFamily="$body">
-            Steps
-          </SizableText>
-        </Tabs.Tab>
-        <Tabs.Tab flex={1} value="tab3">
-          <SizableText fontFamily="$body">
-            Nutrition
-          </SizableText>
-        </Tabs.Tab>
-      </Tabs.List>
-      <Separator />
-      <Tabs.Content value="tab1" f={1}>
-        <FlatList
-          data={ingredients || []}
-          keyExtractor={(item: any) => item._id}
-          renderItem={({ item }) => {
-            return (
-              <XStack f={1} paddingVertical={8}>
-                <Text color={'$gray2Dark'} fontSize={17}>
-                  {item.ingredient.name}
-                </Text>
-              </XStack>
-            );
-          }}
-        />
-      </Tabs.Content>
+      <YStack>
+        {/* <AnimatePresence>
+          {intentAt && (
+            <TabsRovingIndicator
+              width={intentAt.width}
+              height={intentAt.height}
+              x={intentAt.x}
+              y={intentAt.y}
+            />
+          )}
+        </AnimatePresence> */}
+        <AnimatePresence>
+          {activeAt && (
+            <TabsRovingIndicator
+              theme="active"
+              active
+              width={activeAt.width}
+              height={activeAt.height}
+              x={activeAt.x}
+              y={activeAt.y}
+            />
+          )}
+        </AnimatePresence>
+        <Tabs.List
+          // separator={<Separator vertical />}
+          //disablePassBorderRadius="bottom"
+          //aria-label="Manage your account"
+          borderColor={'$nutrisiDark'}
+          borderWidth={1}
+          borderRadius={0}
+        >
+          <Tabs.Tab
+            flex={1}
+            value="tab1"
+            unstyled
+            paddingHorizontal="$3"
+            paddingVertical="$2.5"
+            onInteraction={handleOnInteraction}
+          >
+            <Text color={'$gray3Dark'} fontWeight={'700'}>
+              Ingredients
+            </Text>
+          </Tabs.Tab>
+          <Separator
+            backgroundColor={'$nutrisiDark'}
+            borderColor={'$nutrisiDark'}
+            vertical
+          />
+          <Tabs.Tab
+            flex={1}
+            value="tab2"
+            unstyled
+            paddingHorizontal="$3"
+            paddingVertical="$2"
+            onInteraction={handleOnInteraction}
+          >
+            <Text color={'$gray3Dark'} fontWeight={'700'}>
+              Steps
+            </Text>
+          </Tabs.Tab>
+          <Separator
+            backgroundColor={'$nutrisiDark'}
+            borderColor={'$nutrisiDark'}
+            vertical
+            paddingHorizontal={0}
+            marginHorizontal={0}
+          />
+          <Tabs.Tab
+            flex={1}
+            value="tab3"
+            unstyled
+            paddingHorizontal="$3"
+            paddingVertical="$2"
+            onInteraction={handleOnInteraction}
+          >
+            <Text color={'$gray3Dark'} fontWeight={'700'}>
+              Nutrition
+            </Text>
+          </Tabs.Tab>
+        </Tabs.List>
 
-      <Tabs.Content value="tab2">
-        <FlatList
-          data={steps || []}
-          keyExtractor={(item: any) => item._id}
-          renderItem={({ item }) => {
-            return (
-              <XStack f={1} paddingVertical={8}>
-                <Text color={'$gray2Dark'} fontSize={17}>
-                  {item.name}
-                </Text>
-              </XStack>
-            );
-          }}
-        />
-      </Tabs.Content>
+        <Tabs.Content value="tab1">
+          <FlatList
+            data={ingredients || []}
+            keyExtractor={(item: any) => item._id}
+            renderItem={({ item }) => {
+              return (
+                <XStack f={1} paddingVertical={8}>
+                  <Text color={'$gray2Dark'} fontSize={17}>
+                    {item.ingredient.name}
+                  </Text>
+                </XStack>
+              );
+            }}
+          />
+        </Tabs.Content>
 
-      <Tabs.Content value="tab3">
-        <FlatList
-          data={macros || []}
-          keyExtractor={(item: any) => item._id}
-          renderItem={({ item }) => {
-            return (
-              <XStack f={1} paddingVertical={8}>
-                <Text color={'$gray2Dark'} fontSize={17}>
-                  {item.name}
-                </Text>
-              </XStack>
-            );
-          }}
-        />
-      </Tabs.Content>
+        <Tabs.Content value="tab2">
+          <View>
+            <FlatList
+              data={steps || []}
+              keyExtractor={(item: any) => item._id}
+              renderItem={({ item }) => {
+                return (
+                  <XStack
+                    f={1}
+                    paddingVertical={8}
+                    paddingHorizontal={16}
+                  >
+                    <Text
+                      color={'$gray2Dark'}
+                      fontSize={16}
+                    >
+                      {item.name}
+                    </Text>
+                  </XStack>
+                );
+              }}
+            />
+          </View>
+        </Tabs.Content>
+
+        <Tabs.Content value="tab3">
+          <FlatList
+            data={macros || []}
+            keyExtractor={(item: any) => item._id}
+            renderItem={({ item }) => {
+              return (
+                <XStack f={1} paddingVertical={8}>
+                  <Text color={'$gray2Dark'} fontSize={17}>
+                    {item.name}
+                  </Text>
+                </XStack>
+              );
+            }}
+          />
+        </Tabs.Content>
+      </YStack>
     </Tabs>
   );
 }
 
-const TabsContent = (props: TabsContentProps) => {
+const TabsRovingIndicator = ({
+  active,
+  ...props
+}: { active?: boolean } & StackProps) => {
   return (
-    <Tabs.Content
-      backgroundColor="$background"
-      key="tab3"
-      padding="$2"
-      alignItems="center"
-      height={600}
-      justifyContent="center"
-      borderColor="$background"
-      borderRadius="$2"
-      borderTopLeftRadius={0}
-      borderTopRightRadius={0}
-      borderWidth="$2"
+    <YStack
+      position="absolute"
+      backgroundColor="$nutrisiLight"
+      //animation="100ms"
+      enterStyle={{
+        opacity: 0,
+      }}
+      exitStyle={{
+        opacity: 0,
+      }}
+      {...(active && {
+        backgroundColor: '$nutrisiDark',
+        opacity: 0.6,
+      })}
       {...props}
-    >
-      {props.children}
-    </Tabs.Content>
+    />
   );
 };
+
+const AnimatedYStack = styled(YStack, {
+  variants: {
+    isLeft: { true: { x: -25, opacity: 0 } },
+    isRight: { true: { x: 25, opacity: 0 } },
+    defaultFade: { true: { opacity: 0 } },
+  } as const,
+});
 
 // function DetailsList({ data }: any) {
 //   console.log('data', data);
