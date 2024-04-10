9f1379e14ba913fe178f549bc2d8ee50d097af18
Iulia Puiu <48315224+puiu-iulia@users.noreply.github.com>
2024-04-10 20:40:44 +0300

    Refactor account screen and add logout and delete account functionality

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,27 +1,84 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { View, Text, Separator } from 'tamagui';
+import { Pressable } from 'react-native';
+import { useDispatch } from 'react-redux';
 
-export default function TabTwoScreen() {
+import ThemedScreen from '@/components/screen';
+import Header from '@/components/Header';
+import ConfirmationDialog from '@/components/confirmationDialog';
+import {
+  logoutUser,
+  deleteUser,
+} from '@/store/auth/actions';
+import { useRouter } from 'expo-router';
+
+export default function AccountScreen() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  function handleLogout() {
+    // @ts-ignore
+    dispatch(logoutUser());
+    router.push('/login');
+  }
+
+  function openDialog() {
+    setIsDialogOpen(true);
+  }
+
+  function closeDialog() {
+    setIsDialogOpen(false);
+  }
+
+  function handleDeleteAccount() {
+    // @ts-ignore
+    dispatch(deleteUser());
+    closeDialog();
+    router.push('/register');
+  }
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <Text style={styles.title}>Tab Two</Text>
-    </View>
+    <ThemedScreen>
+      <Header title="Account" />
+      <AccountAction
+        title="Sign Out"
+        onPress={handleLogout}
+      />
+      <Separator />
+      <AccountAction
+        title="Delete Account"
+        onPress={openDialog}
+      />
+      <ConfirmationDialog
+        open={isDialogOpen}
+        confirmationText="Delete Account"
+        acceptTextDescription="Are you sure you want to delete your account?"
+        acceptText="Delete"
+        rejectText="Cancel"
+        onReject={closeDialog}
+        onAccept={handleDeleteAccount}
+      />
+    </ThemedScreen>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+function AccountAction({
+  title,
+  onPress,
+}: {
+  title: string;
+  onPress: () => void;
+}) {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={{
+        paddingVertical: 16,
+      }}
+    >
+      <Text fontSize={20} color={'$nutrisi'}>
+        {title}
+      </Text>
+    </Pressable>
+  );
+}
